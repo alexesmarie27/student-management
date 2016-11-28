69bb162f0210d482165cb3a4760d7f9516d1b577
Alexes <ampress727@yahoo.com>
2016-11-27 22:36:42 -0600

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create table students (
+CREATE TABLE students (
 	snum INTEGER AUTO_INCREMENT, 
 	fname VARCHAR(30), 
 	lname VARCHAR(30),
@@ -6,17 +6,23 @@ create table students (
 	PRIMARY KEY(snum)
 );
 
-create table classes (
+INSERT INTO students (fname, lname, year) VALUES ("Alexes", "Presswood", "Senior");
+
+CREATE TABLE classes (
 	cnum VARCHAR(10), 
 	cname VARCHAR(50),
 	instructor VARCHAR(75),
 	PRIMARY KEY(cnum)
 );
 
-create table enrolled_students (
+INSERT INTO classes (cnum, cname, instructor) VALUES ("CS1000", "Intro to CS", "Michael Jurzcyk");
+
+CREATE TABLE enrolled_students (
 	snum INTEGER, 
 	cnum VARCHAR(10), 
 	FOREIGN KEY(snum) REFERENCES students(snum), 
 	FOREIGN KEY(cnum) REFERENCES classes(cnum),
 	PRIMARY KEY(snum, cnum)
 );
+
+INSERT INTO enrolled_students (snum, cnum) VALUES (1, "CS1000");
